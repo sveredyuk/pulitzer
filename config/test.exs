cb5f9676b6dd63684ef05409b7cddda1683e6a0b
Volodya Sveredyuk <sveredyuk@gmail.com>
2021-02-21 17:11:32 +0200

    fix Ci 1

--- config/test.exs
+++ config/test.exs
@@ -16,6 +16,12 @@ else
     ownership_timeout: 999_999_999
 end
 
+if System.get_env("GITHUB_ACTIONS") do
+  config :elixirius, Elixirius.Repo,
+    username: "postgres",
+    password: "postgres"
+end
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :pulitzer, PulitzerWeb.Endpoint,
